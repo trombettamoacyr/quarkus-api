bbeef70c3f29ca0a31e2a1ef75977186ce762307
Moacyr Jr Trombetta <junior.trombetta@gmail.com>
2022-01-12 23:53:32 -0300

    creation service

--- src/main/java/org/com/controller/UserController.java
+++ src/main/java/org/com/controller/UserController.java
@@ -1,33 +1,41 @@
 package org.com.controller;
 
+import org.com.controller.request.UserRequest;
 import org.com.entity.UserEntity;
+import org.com.services.UserService;
 
-import javax.transaction.Transactional;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
+import javax.inject.Inject;
 import javax.ws.rs.Produces;
+import javax.ws.rs.POST;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
+import java.util.UUID;
 
 @Path("/users")
 public class UserController {
 
+    @Inject
+    UserService userService;
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    public UserEntity create(UserRequest userRequest) {
+        return userService.create(userRequest);
+    }
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<UserEntity> findAllUsers() {
-        return UserEntity.listAll();
+        return userService.findAll();
     }
 
-    @POST
-    @Transactional
+    @GET
+    @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public UserEntity newUser() {
-        var newUser = new UserEntity();
-        newUser.name = "Name";
-        newUser.email = "user@email.com";
-        newUser.persist();
-
-        return newUser;
+    public UserEntity findById(@PathParam("id") UUID id) {
+        return userService.findById(id);
     }
 }
---
+++ src/main/java/org/com/controller/request/UserRequest.java
@@ -0,0 +1,21 @@
+package org.com.controller.request;
+
+public class UserRequest {
+
+    private final String name;
+
+    private final String email;
+
+    public UserRequest(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
