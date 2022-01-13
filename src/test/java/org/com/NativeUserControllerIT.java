73c21bda3bf9f9cbaa54279de2a5a9a3c0861bb6
Moacyr Jr Trombetta <junior.trombetta@gmail.com>
2022-01-13 00:30:11 -0300

    README

--- src/test/java/org/com/NativeUserControllerIT.java
+++
@@ -1,9 +0,0 @@
-package org.com;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeUserControllerIT extends UserControllerTest {
-
-    // Execute the same tests but in native mode.
-}
