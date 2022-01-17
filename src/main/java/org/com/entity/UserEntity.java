4a969391d860d02bbb0b17aff1a6c02677925f41
Moacyr Jr Trombetta <junior.trombetta@gmail.com>
2022-01-16 23:35:50 -0300

    changes in UserEntity and UserService

--- src/main/java/org/com/entity/UserEntity.java
+++ src/main/java/org/com/entity/UserEntity.java
@@ -3,14 +3,18 @@ package org.com.entity;
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.UUID;
 
 @Entity
+@Table(name = "user")
 public class UserEntity extends PanacheEntityBase {
 
     @Id
-    public UUID id = UUID.randomUUID();
+    @GeneratedValue
+    public UUID id;
 
     public String name;
 
